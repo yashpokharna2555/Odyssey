effeb2c88263edcd2bcf46ec30d573d6ff5d20f7
Chandan Dhamande <chandan@Chandans-Macbook-Pro.local>
2025-10-25 12:57:39 -0700

    basic landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import './App.css'
-import Landing from './components/Landing'
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
-    <>
-      <Landing />
-    </>
+    <div className="relative min-h-screen bg-black">
+      <NavBar />
+
+      {/* Centered Odyssey text on the left side */}
+      <div className="pl-40 absolute inset-0 flex items-center">
+        <h1 className="font-vogue text-[10rem] tracking-tight text-white">
+          Odyssey
+        </h1>
+      </div>
+    </div>
   )
 }
 
